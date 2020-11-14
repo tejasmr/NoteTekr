5f0da1476f7885d7d3aaf1413216854455616402
tezz-io <matblockgreninja@gmail.com>
2020-11-14 10:59:07 +0530

    Initial Commit

---
+++ NoteTekr/NoteTekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTekrApp.swift
+//  NoteTekr
+//
+//  Created by Tejas M R on 14/11/20.
+//
+
+import SwiftUI
+
+@main
+struct NoteTekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
